b77390e388eaf0a55e13b8b267d353898a130af5
Otavio Ho Aragoni <otavio.aragoni@gmail.com>
2020-07-03 12:06:06 -0300

    Add routes logic

--- src/routes/transaction.routes.ts
+++ src/routes/transaction.routes.ts
@@ -1,23 +1,31 @@
 import { Router } from 'express';
 
-// import TransactionsRepository from '../repositories/TransactionsRepository';
-// import CreateTransactionService from '../services/CreateTransactionService';
+import TransactionsRepository from '../repositories/TransactionsRepository';
+import CreateTransactionService from '../services/CreateTransactionService';
 
 const transactionRouter = Router();
 
-// const transactionsRepository = new TransactionsRepository();
+const transactionsRepository = new TransactionsRepository();
 
 transactionRouter.get('/', (request, response) => {
   try {
-    // TODO
+    const repositories = transactionsRepository.all();
+    return response.json({
+      repositories,
+      balance: transactionsRepository.getBalance(),
+    });
   } catch (err) {
     return response.status(400).json({ error: err.message });
   }
 });
 
 transactionRouter.post('/', (request, response) => {
+  const { body } = request;
   try {
-    // TODO
+    const transaction = new CreateTransactionService(
+      transactionsRepository,
+    ).execute(body);
+    return response.json(transaction);
   } catch (err) {
     return response.status(400).json({ error: err.message });
   }
